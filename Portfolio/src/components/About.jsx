999085cd69e1763395311732999dbfa5cc6d3db8
ananya36 <ananyabundela23@gmail.com>
2024-07-19 21:55:45 +0530

    updated about.jsx

--- Portfolio/src/components/About.jsx
+++ Portfolio/src/components/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
           <div className="text-rectangle p-4 bg-lightest">
             <h1 className="text-4xl font-bold mb-4">About Me</h1>
             <p className="text-lg">
-              Here is a brief introduction about myself. I am a passionate developer with skills in various technologies including React, Node.js, and more. I enjoy building visually appealing and highly functional web applications.
+              Here is a brief introduction about myself. I am a passionate developer with skills in various technologies including React, Node.js,Next.js and more. I enjoy building visually appealing and highly functional web applications
             </p>
           </div>
         </div>
